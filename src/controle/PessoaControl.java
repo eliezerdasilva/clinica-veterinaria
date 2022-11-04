99585482c47e107fd7c274165d825623c08b8818
Eliézer <frete7132@gmail.com>
2022-11-04 11:27:48 -0300

    Atualização

---
+++ src/controle/PessoaControl.java
@@ -0,0 +1,23 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Pessoa;
+
+public class PessoaControl {
+	
+	private ArrayList<Pessoa> tabelaPessoa;
+
+	public PessoaControl() {this.tabelaPessoa = new ArrayList<>();}
+	
+	public boolean inserir(Pessoa p) {
+		
+		return false;
+	}
+	public boolean alterar(Pessoa p) {
+		return false;
+	}
+	public boolean deletar(Pessoa p) {
+		return false;
+	}
+}
---
+++ src/modelo/Pessoa.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import java.sql.Array;
+
+public class Pessoa {
+	
+	public String nome;
+	public  int cpf;
+	public int idade;
+
+	
+	public Pessoa(String nome, int cpf, int idade) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	 
+}
+
+
