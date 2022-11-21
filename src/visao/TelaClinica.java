506b30128ea3f15f6e60423a8650bdbf616e0217
Eliezer <frete7132@gmail.com>
2022-11-21 15:18:23 -0300

    Pratica

--- src/visao/TelaClinica.java
+++ src/visao/TelaClinica.java
@@ -63,6 +63,7 @@ public class TelaClinica extends JFrame {
 				TelaCadastroTutor telaCadastroTutor = new TelaCadastroTutor();
 				telaCadastroTutor.setLocationRelativeTo(null);
 				telaCadastroTutor.setVisible(true);
+				dispose();
 
 			}
 		});
