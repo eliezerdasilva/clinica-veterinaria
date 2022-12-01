226676970235e179517156e063aa15bcc1a660d6
Eliezer <frete7123@gmail.com>
2022-12-01 13:58:57 -0300

    Pratica

--- src/visao/TelaCadastroTutor.java
+++ src/visao/TelaCadastroTutor.java
@@ -24,34 +24,34 @@ public class TelaCadastroTutor extends JFrame {
 
 	private JPanel contentPane;
 
-	private JTextField nome;
+	private JTextField txtnome;
 
-	private JTextField cpf;
+	private JTextField txtcpf;
 
 	public TutorControl bancoTutor;
 
-	private JTextField cep;
+	private JTextField txtcep;
 
 	private JLabel lblNewLabel_2;
 
-	private JTextField rua;
+	private JTextField txtrua;
 
 	private JLabel lblNewLabel_3;
-	private JTextField textField_2;
+	private JTextField txtn_casa;
 
 	private JLabel numcasa;
 
-	private JLabel lblNewLabel_4;
+	private JLabel lblTituloTutor;
 	private JButton btnNewButton_2;
 	private JButton bnbInserir;
+	private Tutor tutorAEditar;
 
 	Tutor tutor;
 	TutorControl tutorControl;
 
-	/**
-	 * Create the frame.
-	 */
-	public TelaCadastroTutor() {
+	public TelaCadastroTutor(Tutor tutorEdit) {
+		this.tutorAEditar = tutorEdit;
+
 		bancoTutor = TutorControl.getIntancia();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 240, 365);
@@ -61,11 +61,10 @@ public class TelaCadastroTutor extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		nome = new JTextField();
-		nome.setText("Eli");
-		nome.setBounds(63, 47, 86, 20);
-		contentPane.add(nome);
-		nome.setColumns(10);
+		txtnome = new JTextField();
+		txtnome.setBounds(63, 47, 86, 20);
+		contentPane.add(txtnome);
+		txtnome.setColumns(10);
 
 		JLabel lblNewLabel = new JLabel("Nome :");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -78,86 +77,90 @@ public class TelaCadastroTutor extends JFrame {
 		lblNewLabel_1.setBounds(20, 78, 46, 14);
 		contentPane.add(lblNewLabel_1);
 
-		cpf = new JTextField();
-		cpf.setText("312312");
-		cpf.setBounds(63, 78, 86, 20);
-		contentPane.add(cpf);
-		cpf.setColumns(10);
-		cep = new JTextField();
-		cep.setText("cep");
-		cep.setBounds(63, 132, 86, 20);
-		contentPane.add(cep);
-		cep.setColumns(10);
+		txtcpf = new JTextField();
+		txtcpf.setBounds(63, 78, 86, 20);
+		contentPane.add(txtcpf);
+		txtcpf.setColumns(10);
+		txtcep = new JTextField();
+		txtcep.setBounds(63, 132, 86, 20);
+		contentPane.add(txtcep);
+		txtcep.setColumns(10);
 
 		lblNewLabel_2 = new JLabel("Cep :");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblNewLabel_2.setBounds(20, 134, 46, 14);
 		contentPane.add(lblNewLabel_2);
 
-		rua = new JTextField();
-		rua.setText("4341");
-		rua.setBounds(63, 103, 86, 20);
-		contentPane.add(rua);
-		rua.setColumns(10);
+		txtrua = new JTextField();
+		txtrua.setBounds(63, 103, 86, 20);
+		contentPane.add(txtrua);
+		txtrua.setColumns(10);
 
 		lblNewLabel_3 = new JLabel("Rua :");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblNewLabel_3.setBounds(22, 109, 34, 14);
 		contentPane.add(lblNewLabel_3);
 
-		textField_2 = new JTextField();
-		textField_2.setText("123");
-		textField_2.setBounds(63, 163, 86, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		txtn_casa = new JTextField();
+		txtn_casa.setBounds(63, 163, 86, 20);
+		contentPane.add(txtn_casa);
+		txtn_casa.setColumns(10);
 
 		numcasa = new JLabel(" N° :");
 		numcasa.setFont(new Font("Tahoma", Font.BOLD, 13));
 		numcasa.setBounds(22, 165, 34, 14);
 		contentPane.add(numcasa);
 
-		JButton btnNewButton = new JButton("CADASTRAR");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnCadastrarTutor = new JButton("CADASTRAR");
+		btnCadastrarTutor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				String nomeTutorStr = nome.getText();
-				String cpfTutorStr = cpf.getText();
+				if (tutorAEditar != null) {
+					// EDITAR
 
-				Tutor novoTutor = new Tutor();
-
-				if (nomeTutorStr.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Nenhum nome preenchido!");
-				} else {
-					novoTutor.setNome(nomeTutorStr);
-				}
-
-				if (cpfTutorStr.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Nenhum CPF preenchido!");
-				} else {
-					novoTutor.setCpf(Long.valueOf(cpfTutorStr));
-				}
-
-				boolean inserir = bancoTutor.inserir(novoTutor);
-
-				if (inserir == true) {
-					JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
-					tutor = novoTutor;
+					TutorControl control = TutorControl.getIntancia();
+					control.alterar(tutorEdit, tutorEdit.getCpf());
 
 				} else {
-					JOptionPane.showMessageDialog(null, "Erro ao cadastrar!");
-				}
+					// CADASTRAR
+					String nomeTutorStr = txtnome.getText();
+					String cpfTutorStr = txtcpf.getText();
 
+					Tutor novoTutor = new Tutor();
+
+					if (nomeTutorStr.isEmpty()) {
+						JOptionPane.showMessageDialog(null, "Nenhum nome preenchido!");
+					} else {
+						novoTutor.setNome(nomeTutorStr);
+					}
+
+					if (cpfTutorStr.isEmpty()) {
+						JOptionPane.showMessageDialog(null, "Nenhum CPF preenchido!");
+					} else {
+						novoTutor.setCpf(Long.valueOf(cpfTutorStr));
+					}
+
+					boolean inserir = bancoTutor.inserir(novoTutor);
+
+					if (inserir == true) {
+						JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+						tutor = novoTutor;
+
+					} else {
+						JOptionPane.showMessageDialog(null, "Erro ao cadastrar!");
+					}
+				}
 			}
 
 		});
-		btnNewButton.setBounds(50, 196, 120, 23);
-		contentPane.add(btnNewButton);
+		btnCadastrarTutor.setBounds(50, 196, 120, 23);
+		contentPane.add(btnCadastrarTutor);
 
-		lblNewLabel_4 = new JLabel("Cadastro Cliente");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel_4.setForeground(new Color(0, 0, 0));
-		lblNewLabel_4.setBounds(63, 11, 180, 14);
-		contentPane.add(lblNewLabel_4);
+		lblTituloTutor = new JLabel("Cadastro Cliente");
+		lblTituloTutor.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblTituloTutor.setForeground(new Color(0, 0, 0));
+		lblTituloTutor.setBounds(63, 11, 180, 14);
+		contentPane.add(lblTituloTutor);
 
 		JButton btnNewButton_1 = new JButton("Voltar ");
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -197,6 +200,16 @@ public class TelaCadastroTutor extends JFrame {
 		});
 		bnbInserir.setBounds(48, 232, 122, 23);
 		contentPane.add(bnbInserir);
+
+		if (tutorAEditar != null) {
+
+			txtnome.setText(tutorAEditar.getNome());
+			lblTituloTutor.setText("Atualizar tutor");
+			btnCadastrarTutor.setText("Atualizar");
+
+			// SETAR OS DADOS
+
+		}
 
 	}
 }
